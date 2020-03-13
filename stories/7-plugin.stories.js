35d5ee24ffb1b83b6e6c28ec7fa1701f61cf5273
anjjeee <anjjeee.11.17@gmail.com>
2020-03-13 14:49:11 +0900

    가이드 추가

--- stories/7-plugin.stories.js
+++ stories/7-plugin.stories.js
@@ -106,11 +106,6 @@ $('datetimepicker_name').datetimepicker({
   closeOnTimeSelect: true // 시간을 선택하면 datetimepicker를 종료합니다.
 });
           `
-        },
-        {
-          title:'window popup',
-          info:'아 귀찮아',
-          code:``
         }
       ],
       codeView:false
@@ -125,16 +120,55 @@ export const smarteditor = () => ({
     </ui-comp>`,
   data(){
     return{
-        parts:[
-          {
-            title:'modal popup',
-            info:'bootstrap에서 제공하는 modal popup의 기본 형태입니다. modal을 실행시킬 button에는 data-toggle="modal"과 data-target="modal의 id"를 추가해줍니다.',
-            code:``
-          },
-          {
-          title:'window popup',
-          info:'아 귀찮아',
-          code:``
+      parts:[
+        {
+          title:'smart editor의 구조',
+          info:'',
+          code:`
+// html
+<textarea id="id" name=""></textarea>
+
+// script
+nhn.husky.EZCreator.createInIFrame({
+  oAppRef: oEditors,
+  elPlaceHolder: "id",
+  sSkinURI: "<?=SmartEditorLang()?>",
+  htParams : {
+    bUseToolbar : true,
+    bUseVerticalResizer : true,
+    bUseModeChanger : true,
+    fOnBeforeUnload : function(){}
+  },
+  fOnAppLoad : function(){
+    // smart editor가 load 된 뒤 실행
+  },
+  fCreator: "createSEditor2"
+});`
+        },
+        {
+          title:'smart editor 응용',
+          info:'smart editor의 높이, 너비를 변형하거나 내용을 추가할 때에는 아래와 같은 방법을 사용해야 합니다.',
+          code:`
+// editor 내용 가져오기
+oEditors.getById["id"].getIR();
+
+// editor 내용 초기화 
+oEditors.getById["id"].exec("SET_IR",[""]);
+
+// editor에 내용 추가
+oEditors.getById["id"].exec("PASTE_HTML",["이 내용을 editor에 자동으로 넣고 싶다."]);
+
+// editor 너비 또는 높이에 일정 값을 추가하고 싶은 경우
+oEditors.getById["id"].exec("MSG_EDITING_AREA_RESIZE_STARTED", []); 
+oEditors.getById["id"].exec("RESIZE_EDITING_AREA_BY", [추가하고 싶은 너비의 px, 추가하고 싶은 높이의 px]); // 숫자만 입력합니다.
+oEditors.getById["id"].exec("MSG_EDITING_AREA_RESIZE_ENDED", []); []); 
+          
+// editor 너비 또는 높이를 특정 값으로 적용하고 싶은 경우
+oEditors.getById["id"].exec("MSG_EDITING_AREA_RESIZE_STARTED", []); 
+oEditors.getById["id"].exec("RESIZE_EDITING_AREA", [적용할 너비의 px, 적용할 높이의 px]); // 숫자만 입력합니다.
+oEditors.getById["id"].exec("MSG_EDITING_AREA_RESIZE_ENDED", []); 
+oEditors.getById["id"].exec("SE_FIT_IFRAME", []); // 에디터를 둘러싼 iframe 사이즈 변경 
+`
         }
       ],
       codeView:false
@@ -151,14 +185,24 @@ export const colorPick = () => ({
     return{
         parts:[
           {
-            title:'modal popup',
-            info:'bootstrap에서 제공하는 modal popup의 기본 형태입니다. modal을 실행시킬 button에는 data-toggle="modal"과 data-target="modal의 id"를 추가해줍니다.',
-            code:``
-          },
-          {
-          title:'window popup',
-          info:'아 귀찮아',
-          code:``
+            title:'colorpicker',
+            info:'사용자가 팔레트에서 색을 선택할 수 있게 합니다.',
+            code:`
+<link href="/css/plugins/colorpicker/bootstrap-colorpicker.min.css" rel="stylesheet" />
+<link href="/css/plugins/colorpicker/bootstrap-colorpicker-plus.css" rel="stylesheet" />
+<script type='text/javascript' src="/js/plugins/colorpicker/bootstrap-colorpicker.min.js"></script>
+<script type='text/javascript' src="/js/plugins/colorpicker/bootstrap-colorpicker-plus.js"></script>
+
+$('button').colorpickerplus();
+$('button').on('changeColor', function(e,color){
+  if(color==null) {
+    // 색 없음 선택
+    $('.color-fill-icon', $(this)).addClass('colorpicker-color');
+  } else {
+    $('.color-fill-icon', $(this)).removeClass('colorpicker-color');
+    $('.color-fill-icon', $(this)).css('background-color', color);
+  }
+});`
         }
       ],
       codeView:false
@@ -173,16 +217,23 @@ export const toastr = () => ({
     </ui-comp>`,
   data(){
     return{
-        parts:[
-          {
-            title:'modal popup',
-            info:'bootstrap에서 제공하는 modal popup의 기본 형태입니다. modal을 실행시킬 button에는 data-toggle="modal"과 data-target="modal의 id"를 추가해줍니다.',
-            code:``
-          },
-          {
-          title:'window popup',
-          info:'아 귀찮아',
-          code:``
+      parts:[
+        {
+          title:'toaster',
+          info:'toaster는 기존의 alert 대신 일정 시간 후 자동으로 사라지게 하는 알림창을 띄울 떄 사용합니다.',
+          code:`
+// 옵션 설정
+toastr.options = {
+  "debug": true,
+  "positionClass": "toast-top-center", // 나타날 위치
+  "preventDuplicates": false,
+  "onclick": null,
+  "timeOut": "5000" // 표현되는 시간
+}
+
+// 사용 예
+toastr.success("띄울 문구");
+`
         }
       ],
       codeView:false
